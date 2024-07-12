7f18c392ba17e09b9eb7e4c6a35890c566227048
PredatorXxRIPxX <wassimou009@gmail.com>
2024-07-13 00:08:23 +0200

    onistep Project

--- src/app/_components/@mainpage/mainpage.tsx
+++ src/app/_components/@mainpage/mainpage.tsx
@@ -1,5 +1,6 @@
 "use client"
 import ReactAwesomeTypewriter from "react-awesome-typewriter";
+import {motion} from "framer-motion";
 
 
 function Mainpage() {
@@ -11,9 +12,15 @@ function Mainpage() {
         [{ text: "Premiere Pro" , classNames: "text-6xl font-bold font-FjallaOne" },],
     ];
 
+
+
     return (
         <div className="text-center mb-20 mt-20">
-            <h1 className="font-bold font-FjallaOne text-6xl text-center text-white">
+            <motion.h1 initial={{opacity:0,x:-200}} animate={{opacity:1,x:0,transition:{
+                delay:1,
+                duration:1
+
+            }}} className="font-bold font-FjallaOne text-6xl text-center text-white">
                 Let us guide your clicks,<br /><br />
                 in <span className="bg-clip-text text-transparent bg-gradient-to-r font-FjallaOne from-[#EB88F6] to-[#EB48A2]">
                     <ReactAwesomeTypewriter
@@ -26,12 +33,22 @@ function Mainpage() {
                         cursorWidth="1.25rem"
                     />
                 </span>
-            </h1>
-            <p className="text-2xl font-medium text-white mt-10 font-varela">
+            </motion.h1>
+            <motion.p initial={{y:-100,opacity:0}} animate={{opacity:1,y:0,transition:{
+                delay:2,
+                duration:1,
+                type:"spring",
+                stiffness:100
+            }}} className="text-2xl font-medium text-white mt-10 font-varela">
                 Onistep is your Software Assistant. Ask it any question 
-            </p>
+            </motion.p>
             <center>
-                <button className="flex items-center mt-10 px-6 py-3 font-bold text-white bg-opacity-20 bg-white rounded-lg shadow-lg backdrop-blur hover:bg-opacity-30 transition-all duration-300">Get it now <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ml-2"><path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" /></svg></button>
+                <motion.button initial={{opacity:0,scale:0}} animate={{opacity:1,scale:1,transition:{
+                    delay:3,
+                    duration:1,
+                    type:"spring",
+                    stiffness:100
+                }}} className="flex items-center mt-10 px-6 py-3 font-bold text-white bg-opacity-20 bg-white rounded-lg shadow-lg backdrop-blur hover:bg-opacity-30 transition-all duration-300">Get it now <motion.svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ml-2"><path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" /></motion.svg></motion.button>
             </center>
         </div>
     );
