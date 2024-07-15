6ac75f844fb6c2ee66c58dd2d13e91a0088a0463
PredatorXxRIPxX <wassimou009@gmail.com>
2024-07-15 15:00:28 +0200

    onistep Project

--- src/app/_components/@mainpage/mainpage.tsx
+++ src/app/_components/@mainpage/mainpage.tsx
@@ -5,25 +5,24 @@ import {motion} from "framer-motion";
 
 function Mainpage() {
     const options = [
-
-        [{ text: "Photoshop", classNames: "text-6xl font-bold font-FjallaOne" },],
-        [{ text: "Unity" , classNames: "text-6xl font-bold font-FjallaOne" },],
-        [{ text: "Blender" , classNames: "text-6xl font-bold font-FjallaOne" },],
-        [{ text: "Premiere Pro" , classNames: "text-6xl font-bold font-FjallaOne" },],
+        [{ text: "Photoshop", classNames: "text-7xl font-medium font-FjallaOne" },],
+        [{ text: "Unity" , classNames: "text-7xl font-medium font-FjallaOne" },],
+        [{ text: "Blender" , classNames: "text-7xl font-medium font-FjallaOne" },],
+        [{ text: "Premiere Pro" , classNames: "text-7xl font-medium font-FjallaOne" },],
     ];
 
 
 
     return (
         <div className="text-center mb-20 mt-20 font-FjallaOne">
-            <motion.h1 className={`font-bold  text-6xl text-center text-white`}>
+            <motion.h1 className={`text-7xl text-center text-white font-medium`}>
                 Let us guide your clicks,<br /><br />
-                in <span className="bg-clip-text text-transparent bg-gradient-to-r font-FjallaOne from-[#EB88F6] to-[#EB48A2]">
+                in <span className="bg-clip-text text-transparent bg-gradient-to-r font-FjallaOne from-[#EB88F6] to-[#EB48A2] font-normal text-8xl">
                     <ReactAwesomeTypewriter
                         options={options}
                         backwardSpeed={80}
                         forwardSpeed={60}
-                        waitTime={2500}
+                        waitTime={1000}
                         cursorColor="#EB48A2"
                         cursorHeight="2.25rem"
                         cursorWidth="1.25rem"
@@ -34,12 +33,7 @@ function Mainpage() {
                 Onistep is your Software Assistant. Ask it any question 
             </motion.p>
             <center>
-                <motion.button initial={{opacity:0,scale:0}} animate={{opacity:1,scale:1,transition:{
-                    delay:.5,
-                    duration:1,
-                    type:"spring",
-                    stiffness:100
-                }}} className="flex items-center mt-10 px-6 py-3 font-bold text-white bg-opacity-20 bg-white rounded-lg shadow-lg backdrop-blur hover:bg-opacity-30 transition-all duration-300">Get it now <motion.svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ml-2"><path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" /></motion.svg></motion.button>
+                <motion.button className="flex items-center mt-10 px-6 py-3 font-bold text-white bg-opacity-20 bg-white rounded-lg shadow-lg backdrop-blur hover:bg-opacity-30 transition-all duration-300">Get it now <motion.svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ml-2"><path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" /></motion.svg></motion.button>
             </center>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,42 +6,20 @@ import { cardData } from "./_data/data";
 import CardContainer from "./_components/@cardContainer/cardcontainer";
 import Footer from "./_components/@footer/footer";
 import Btn from "./_components/btn/btn";
+import dynamic from "next/dynamic";
+
+const CriticalComponents = dynamic(() => import("./@infoPage/page"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
+
+
 
 export default function Home() {
   return (
     <>
       <Mainpage />
-      <div className=" w-full flex items-center justify-center  overflow-x-hidden">
-        <VideoSection props={{url:"45554"}} />
-      </div>
-      <AppSlider />
-      <div className="p-10">
-        <h1 className="text-3xl text-white font-bold text-center font-FjallaOne">HEllo HEllo helloooooo</h1>
-        <Container props={{videoUrl:"", header:"Never leave your window", description:"Onistep brings all the guidance you need right in your software.No more endless searches.Get the answers you need instantly." ,imageRight:false }}/>
-        <div className="text-white">
-          <h1 className="font-bold text-center text-3xl font-FjallaOne">Take a look at our collection of Courses</h1>
-          <div className="h-10"></div>
-          <p className="font-semibold text-center text-xl font-varela">They’re tailor made for newcomer who want practice.</p>
-          <div className=" grid grid-cols-4 grid-rows-1 gap-2 w-full p-4">
-            {
-              cardData.map(({title,imgUrl,iconUrl,level}:any)=>{
-                return <CardContainer props={{title,imgUrl,iconUrl,level}}/>
-              })
-            }
-          </div>
-        </div>
-        <h1 className="text-3xl text-white font-bold text-center font-FjallaOne ">HEllo HEllo helloooooo</h1>
-        <Container props={{videoUrl:"", header:"Twice more accurate than GPT-4o", description:"Onistep brings all the guidance you need right to your screen. No more endless searches.Get the answers you need instantly." ,imageRight:true }}/>
-      </div>
-      <div className="text-center text-white">
-        <p className="font-semibold text-2xl font-varela">What are you waiting for?</p>
-        <div className="h-10"></div>
-        <p className="font-bold text-5xl font-FjallaOne">Try it yourself!</p>
-        <div className="mt-20 bg-gradient-to-r from-[#6A30B2] to-[#170D44] relative p-6 rounded-t-full">
-          <Btn text="Download now" />
-        </div>
-      </div>
-      <Footer />
+      <CriticalComponents />
     </>
   );
 }
