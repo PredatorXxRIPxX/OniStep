eb177161e4fa5775263f5616f080acca45c5e095
Terfi mohammed wassim <99509460+PredatorXxRIPxX@users.noreply.github.com>
2024-07-23 20:17:35 +0200

    Update container.tsx

--- src/app/_components/@container/container.tsx
+++ src/app/_components/@container/container.tsx
@@ -38,9 +38,10 @@ export default function Container({props}:{props:{header:string,description:Reac
             <div ref={currentContainer} className="w-full h-full grid grid-cols-1  lg:grid-rows-1 mt-6 mb-6 lg:grid-cols-5 rounded-3xl p-4 relative overflow-x-hidden border-4 border-solid border-[#ABAFDF] before:content-[''] before:absolute before:inset-0 before:backdrop-blur-xl before:from-[#6C67F2]/50 before:to-[#709E6E]/50 before:z-[-1] before:bg-gradient-to-r">
 
                 <div className={`rounded-lg bg-gradient-to-bl ${props.imageRight&&"order-2"} from-[#170D44] to-[#EB48A2] m-2 col-span-3 border-4 border-solid border-[#ABAFDF] rounded-xl`}>
-                        <video className="w-full h-auto rounded-xl" autoPlay loop muted>
+                    {/*<video className="w-full h-auto rounded-xl" autoPlay loop muted>
                             <source src={props.videoUrl} type="video/mp4" />
-                        </video>
+                        </video>*/}
+                    <Image src={props.videoUrl} alt="image" layout='fill' />
                     
                 </div>
                 <div className={`flex-col h-full w-full items-center font-fjalla justify-around  ${props.imageRight&&"order-1"} text-center col-span-2`}>
