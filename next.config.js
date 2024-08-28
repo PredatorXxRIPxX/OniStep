b73587d3e6725ce0c0388ed7d7a355751a50a700
Terfi mohammed wassim <99509460+PredatorXxRIPxX@users.noreply.github.com>
2024-08-28 15:38:23 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output:"export",
-  basePath: process.env.NODE_ENV === 'production' ? '/OniStep' : '',
 
+const nextConfig = {
+  
+  basePath: process.env.NODE_ENV === 'production' ? '/OniStep' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/OniStep/' : '',
 
   async rewrites() {
     return [
       {
         source: '/:path*',
-        destination: `/:path*`,
+        destination: '/:path*',
       },
     ];
   },
